615cfb1ba7c25c323c98ab079ea9287c046d7c99
jaythe2012 <jaythe2012@gmail.com>
2018-08-11 14:50:51 +0530

    FCM DB Setup

--- app/src/main/java/services/SaveFCMTokenService.java
+++ app/src/main/java/services/SaveFCMTokenService.java
@@ -7,17 +7,10 @@ import android.os.IBinder;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.zxing.common.StringUtils;
 
-import java.security.Key;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.UUID;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicInteger;
-
-import Model.FCM_Device_Tokens;
 
 public class SaveFCMTokenService extends Service {
     int count;
@@ -56,19 +49,65 @@ public class SaveFCMTokenService extends Service {
         return null;
     }
 
-    private void sendRegistrationToServer(final String token) {
+    private boolean sendRegistrationToServer(final String token) {
         // Add custom implementation, as needed.
-
-        DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
-        DatabaseReference fcmDatabaseRef = ref.child("FCM_Device_Tokens");
-
         UUID uuid = UUID.randomUUID();
         String randomUUIDString = uuid.toString();
+        String randomstring = randomUUIDString.substring(0,9);
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
 
-        HashMap<String,Object> myMap = new HashMap<>();
-        myMap.put(randomUUIDString,token);
+        int currentapiVersion = android.os.Build.VERSION.SDK_INT;
+        switch (currentapiVersion) {
 
-        fcmDatabaseRef.updateChildren(myMap);
+            case 23:
+                DatabaseReference api23devices = ref.child("FCM_Device_Tokens").child("API_23_Devices");
+                HashMap<String, Object> api23 = new HashMap<>();
+                api23.put(randomstring, token);
+                api23devices.updateChildren(api23);
+                return true;
+
+            case 24:
+                DatabaseReference api24devices = ref.child("FCM_Device_Tokens").child("API_24_Devices");
+                HashMap<String, Object> api24 = new HashMap<>();
+                api24.put(randomstring, token);
+                api24devices.updateChildren(api24);
+                return true;
+
+            case 25:
+                DatabaseReference api25devices = ref.child("FCM_Device_Tokens").child("API_25_Devices");
+                HashMap<String, Object> api25 = new HashMap<>();
+                api25.put(randomstring, token);
+                api25devices.updateChildren(api25);
+                return true;
+
+            case 26:
+                DatabaseReference api26devices = ref.child("FCM_Device_Tokens").child("API_26_Devices");
+                HashMap<String, Object> api26 = new HashMap<>();
+                api26.put(randomstring, token);
+                api26devices.updateChildren(api26);
+                return true;
+
+            case 27:
+                DatabaseReference api27devices = ref.child("FCM_Device_Tokens").child("API_27_Devices");
+                HashMap<String, Object> api27 = new HashMap<>();
+                api27.put(randomstring, token);
+                api27devices.updateChildren(api27);
+                return true;
+
+            case 28:
+                DatabaseReference api28devices = ref.child("FCM_Device_Tokens").child("API_28_Devices");
+                HashMap<String, Object> api28 = new HashMap<>();
+                api28.put(randomstring, token);
+                api28devices.updateChildren(api28);
+                return true;
+
+            default:
+                DatabaseReference unknown = ref.child("FCM_Device_Tokens").child("Unknown_Devices");
+                HashMap<String, Object> unknownapi = new HashMap<>();
+                unknownapi.put(randomstring, token);
+                unknown.updateChildren(unknownapi);
+                return true;
+        }
     }
 }
 
